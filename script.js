21c6410d7d3e79e2fa3badf4d06b4ed4ff4799c8
Ultrathor4 <danilo.gomes.torquato@gmail.com>
2022-06-11 20:10:31 -0300

    código implementado com sucesso

--- script.js
+++ script.js
@@ -68,14 +68,13 @@ let marker = x;
 let vencedor = 0;
 
 const jogoDaVelha = new Map([
-    ["cel00", 0], ["cel01", 0], ["cel02", 0],
-    ["cel10", 0], ["cel11", 0], ["cel12", 0],
-    ["cel20", 0], ["cel21", 0], ["cel22", 0],
+    ["cel00", 0], ["cel01", 1], ["cel02", 2],
+    ["cel10", 3], ["cel11", 4], ["cel12", 5],
+    ["cel20", 6], ["cel21", 7], ["cel22", 8],
 ]);
 
-function marcador (elem){
-
-    if(jogoDaVelha.has(elem.id)){
+function marcador(elem) {
+    if (jogoDaVelha.has(elem.id) && vencedor == 0 && elem.innerHTML != "X" && elem.innerHTML != "O") {
         elem.innerHTML = marker;
         if (marker == x) {
             marker = o;
@@ -83,24 +82,28 @@ function marcador (elem){
             marker = x;
         }
         jogoDaVelha.set(elem.id, elem.innerHTML)
+        console.log(jogoDaVelha);
 
-        for(let i = 0; i < 3; i++){
-            if(jogoDaVelha.get(`cel${i}0`) == "X" && jogoDaVelha.get(`cel${i}1`) == "X" && jogoDaVelha.get(`cel${i}2`) == "X"){
+        for (let i = 0; i < 3; i++) {
+            if (jogoDaVelha.get(`cel${i}0`) == elem.innerHTML && jogoDaVelha.get(`cel${i}1`) == elem.innerHTML && jogoDaVelha.get(`cel${i}2`) == elem.innerHTML) {
+                vencedor = 1;
+                alert("Temos um vencedor!")
+            } else if (jogoDaVelha.get(`cel0${i}`) == elem.innerHTML && jogoDaVelha.get(`cel1${i}`) == elem.innerHTML && jogoDaVelha.get(`cel2${i}`) == elem.innerHTML) {
                 vencedor = 1;
                 alert("Temos um vencedor!")
             }
         }
-        for(let i = 0; i < 3; i++){
-            if(jogoDaVelha.get(`cel0${i}`) == "X" && jogoDaVelha.get(`cel1${i}`) == "X" && jogoDaVelha.get(`cel2${i}`) == "X"){
-                vencedor = 1;
-                alert("Temos um vencedor!")
-            }
+        if (jogoDaVelha.get("cel00") == elem.innerHTML && jogoDaVelha.get("cel11") == elem.innerHTML && jogoDaVelha.get("cel22") == elem.innerHTML) {
+            vencedor = 1;
+            alert("Temos um vencedor!")
         }
-
+        if (jogoDaVelha.get("cel20") == elem.innerHTML && jogoDaVelha.get("cel11") == elem.innerHTML && jogoDaVelha.get("cel02") == elem.innerHTML) {
+            vencedor = 1;
+            alert("Temos um vencedor!")
+        }
     } else if (vencedor == 1) {
         alert("Opa...já temos um vencedor!")
     } else {
         alert("Opção indisponível. Para continuar, selecione um espaço vazio!")
     }
-
 }
